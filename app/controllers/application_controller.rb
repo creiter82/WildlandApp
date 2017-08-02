e1c3402de3b597271155b66cb9465612fa83e418
Cameron Reiter <reiter303@hotmail.com>
2017-08-01 21:36:31 -0600

    Add controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include DeviseWhitelist
 
-  before_filter :configure_permitted_params, if: :devise_controller?
-
-  def configure_permitted_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :shift])
-    # For editing users
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :shift])
-  end
 end
---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_params, if: :devise_controller?
+  end
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :shift])
+    # For editing users
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :shift])
+  end
+
+end
