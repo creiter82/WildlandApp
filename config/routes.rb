6af2d0145b034fcd3c5415452901a7f3a4b24137
Cameron Reiter <reiter303@hotmail.com>
2017-08-14 16:23:20 -0600

    Fix spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :assignments, only: [:new, :create, :destroy]
+  resources :availabilities, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
