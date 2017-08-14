833bed8fb1e639e00af656a20e59eec300adfdc1
Cameron Reiter <reiter303@hotmail.com>
2017-08-14 15:21:02 -0600

    Add create and destroy actions for availabilities

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,9 +3,23 @@ class AvailabilitiesController < ApplicationController
   end
 
   def create
+    @availability = Availability.new(availability_params)
+
+    if @availability.save
+      flash[:notice] = "You are signed up"
+      redirect_to :back
+    else
+      flash[:danger] = "You were not signed up"
+      redirect_to :back
+    end
   end
 
   def destroy
+    @availability = Availability.find(params[:id])
+
+    if @availability.destroy
+      flash[:notice] = "You were removed from the Roster"
+    end
   end
 
   private
