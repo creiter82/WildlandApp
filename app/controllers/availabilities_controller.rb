07090f8e1f3a242071c7cffd0481c188817f6544
Cameron Reiter <reiter303@hotmail.com>
2017-08-14 15:07:37 -0600

    Whitelist availability params

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -7,4 +7,11 @@ class AvailabilitiesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def availability_params
+      params.require(:availability).permit(:roster_id, :user_id)
+    end
+
 end
