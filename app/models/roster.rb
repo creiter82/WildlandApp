7bd8466055206dbea1b5d777ce8f24ac3a1a5720
Cameron Reiter <reiter303@hotmail.com>
2017-08-16 19:47:52 -0600

    Fix association with users

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -6,7 +6,7 @@ class Roster < ApplicationRecord
   has_many :users, through: :availabilities
 
   has_many :team_deployments
-  has_many :rosters, through: :team_deployments
+  has_many :deployments, through: :team_deployments
 
   def set_availability_position position
     availabilities.where(availability_position_id: position.id).order(:created_at)
