8b154dd78a8bf4610a43b379f39bc3f64978178b
Cameron Reiter <reiter303@hotmail.com>
2017-08-16 19:53:53 -0600

    Add user ids and roster ids to deployments params

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -69,6 +69,6 @@ class DeploymentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def deployment_params
-      params.require(:deployment).permit(:name, :location, :slug)
+      params.require(:deployment).permit(:name, :location, :slug, roster_ids:[], user_ids:[])
     end
 end
