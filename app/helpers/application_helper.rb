baf021e1bdabfffd3cfd75b2035fff93e7d0b5d0
Cameron Reiter <reiter303@hotmail.com>
2018-01-03 09:30:12 -0700

    Update roster helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def index_links var
-    (link_to fa_icon("eye", class: 'fa-lg'), var) + " ".html_safe +
-    (link_to fa_icon("pencil-square-o", class: 'fa-lg'), edit_roster_path(var), class: 'edit-btn') + " ".html_safe + 
-    (link_to fa_icon("trash-o", class: 'fa-lg'), var, method: :delete, data: { confirm: 'Are you sure?' }, class: 'delete-btn') 
-  end
+  
 
 end
