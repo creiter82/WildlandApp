6dd858c5d66087c699c2a8ce7ba8482b2c04c55d
Cameron Reiter <reiter303@hotmail.com>
2018-02-05 11:52:21 -0700

    Update layout and supporting files for Flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
-  
+  def flash_class(level)
+      case level
+          when 'notice'  then "alert alert-info"
+          when 'success' then "alert alert-success"
+          when 'danger'  then "alert alert-danger"  
+          when 'error'   then "alert alert-error"
+          when 'alert'   then "alert alert-error"
+      end
+  end
 
 end
