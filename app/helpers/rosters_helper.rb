36a041caa05b177bdc592a44fbf45917670e1a27
Cameron Reiter <reiter303@hotmail.com>
2018-03-06 18:26:54 -0700

    Use authentication for link visibility

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -1,7 +1,7 @@
 module RostersHelper
 
   def roster_index_links var
-    (link_to fa_icon("eye", class: 'fa-lg'), var) + " ".html_safe +
+    # (link_to fa_icon("eye", class: 'fa-lg'), var) + " ".html_safe +
     (link_to fa_icon("pencil-square-o", class: 'fa-lg'), edit_roster_path(var), class: 'edit-btn') + " ".html_safe + 
     (link_to fa_icon("trash-o", class: 'fa-lg'), var, method: :delete, data: { confirm: 'Are you sure?' }, class: 'delete-btn') 
   end
