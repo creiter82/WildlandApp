cb12be97a685ed42656595dbd20e2200cbfb108a
Cameron Reiter <reiter303@hotmail.com>
2018-03-25 07:31:20 -0600

    Clean up duplicate resource for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :users, path: 'personnel', only: [:edit, :update, :show, :destroy]
-  resources :users, path: 'personnel', only: [:index]
+  resources :users, path: 'personnel', only: [:index, :edit, :update, :show, :destroy]
 
   resources :rosters
   
@@ -16,5 +15,4 @@ Rails.application.routes.draw do
 
   resources :availabilities, only: [:new, :create, :destroy]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
