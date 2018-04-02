1cc5ebd42b56da5b886eebb1125147f79478060b
Cameron Reiter <reiter303@hotmail.com>
2018-04-01 21:09:28 -0600

    Fix calendar styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :users, path: 'personnel', only: [:index, :edit, :update, :show, :destroy]
   
+  resources :users, path: 'personnel', only: [:index, :edit, :update, :show, :destroy]
+
   resources :rosters
   
   resources :deployments do
