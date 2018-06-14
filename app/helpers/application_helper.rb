5fafe5c211bebbeb8c379bf97aefea458b5080be
Cameron Reiter <reiter303@hotmail.com>
2018-06-14 11:15:29 -0600

    Refactor days_deployed to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
       end
   end
 
+  def days_deployed deployment
+    if deployment.returned_date?
+      (deployment.returned_date - deployment.created_at.to_date + 1).to_i
+    else
+      (Date.today - deployment.created_at.to_date + 1).to_i  
+    end  
+  end
+
 end
--- app/helpers/deployments_helper.rb
+++ app/helpers/deployments_helper.rb
@@ -1,11 +1,4 @@
 module DeploymentsHelper
 
-  def days_deployed deployment
-    if deployment.returned_date?
-      (deployment.returned_date - deployment.created_at.to_date + 1).to_i
-    else
-      (Date.today - deployment.created_at.to_date + 1).to_i  
-    end  
-  end
 end
 
