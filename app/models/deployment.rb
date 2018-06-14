091b70cfa3a21c47be23c100e4c3dad3b8c882f7
Cameron Reiter <reiter303@hotmail.com>
2018-06-14 10:58:00 -0600

    Fix bug with updating deployment names and slugs

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -10,4 +10,16 @@ class Deployment < ApplicationRecord
   mount_uploaders :images, ImageUploader
 
   scope :from_this_year, lambda { where("deployments.created_at > ? AND deployments.created_at < ?", Time.current.beginning_of_year, Time.current.end_of_year) }
+
+  private
+    def should_generate_new_friendly_id?
+      name_changed?
+    end
+
+    def slug=(value)
+      if value.present?
+        write_attribute(:slug, value)
+      end
+    end
+
 end
